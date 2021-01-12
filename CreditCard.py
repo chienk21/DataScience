ed8266ec4cdc6b9d9825e6ad6dc765b2ad2a52f9
chienk21 <64667241+chienk21@users.noreply.github.com>
2021-01-11 19:12:51 -0600

    second commit

--- CreditCard.py
+++ CreditCard.py
@@ -5,7 +5,15 @@ Then shows whether it is valid for American Express, MasterCard, Visa, or Discov
 
 def creditCard():
     credit_card_number = input("What is your credit card number? ")
+
+    #retrieving the first digit or zeroth index of the credit card number
     first_digit_card = credit_card_number[0]
+
+    #The user's input is considered a string, so comparison needs to be with a string/char not int
+    #The first digit of mastercards is 2 or 5
+    #The first digit of american express cards is 3 
+    #The first digit of visas is 4
+    #The first digit of discover cards is 6
     if first_digit_card == '2' or first_digit_card == '5':
         print("Your credit card is a valid MasterCard!")
     elif first_digit_card == '3':
