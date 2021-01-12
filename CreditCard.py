4d1f3eb82dd5336581f20fcac2965cfe52367779
chienk21 <64667241+chienk21@users.noreply.github.com>
2021-01-11 19:20:59 -0600

    third commit

--- CreditCard.py
+++ CreditCard.py
@@ -5,7 +5,7 @@ Then shows whether it is valid for American Express, MasterCard, Visa, or Discov
 
 def creditCard():
     credit_card_number = input("What is your credit card number? ")
-    
+
     #retrieving the first digit or zeroth index of the credit card number
     first_digit_card = credit_card_number[0]
 
@@ -14,14 +14,17 @@ def creditCard():
     #The first digit of american express cards is 3 
     #The first digit of visas is 4
     #The first digit of discover cards is 6
-    if first_digit_card == '2' or first_digit_card == '5':
-        print("Your credit card is a valid MasterCard!")
-    elif first_digit_card == '3':
-        print("Your credit card is a valid American Express Card!")
-    elif first_digit_card == '4':
-        print("Your credit card is a valid Visa Card!")
-    elif first_digit_card == '6': 
-        print("Your credit card is a valid Discover Card!")
+    if len(credit_card_number) == 16 or len(credit_card_number) == 15:
+        if first_digit_card == '2' or first_digit_card == '5':
+            print("Your credit card is a valid MasterCard!")
+        elif first_digit_card == '3':
+            print("Your credit card is a valid American Express Card!")
+        elif first_digit_card == '4':
+            print("Your credit card is a valid Visa Card!")
+        elif first_digit_card == '6': 
+            print("Your credit card is a valid Discover Card!")
+        else:
+            print("This is not a valid credit card number.")
     else:
         print("This is not a valid credit card number.")
 
