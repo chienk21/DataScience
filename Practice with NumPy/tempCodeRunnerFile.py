9ed08e9778489cc5a755b8b5d3e660463d3c7207
chienk21 <chienk21@greenhill.org>
2021-03-05 13:11:34 -0600

    commit 3/5

---
+++ Practice with NumPy/tempCodeRunnerFile.py
@@ -0,0 +1,10 @@
+import numpy as geek 
+
+in_arr1 = geek.array([[2, -7, 5], [-6, 2, 0]]) 
+in_arr2 = geek.array([[5, 8, -5], [3, 6, 9]]) 
+
+print ("1st Input array : ", in_arr1) 
+print ("2nd Input array : ", in_arr2) 
+	
+out_arr = geek.add(in_arr1, in_arr2) 
+print ("output added array : ", out_arr) 
